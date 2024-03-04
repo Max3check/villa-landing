57c1e7fa00650d3a4b68f3e3ec39205007c85e58
Maxim <maxtab1@mail.ru>
2024-03-04 19:46:22 +0300

    tuning custom sllider

--- script/overview/customSlider_2.js
+++ script/overview/customSlider_2.js
@@ -1,2 +1,49 @@
 const slides = document.querySelector(".slides");
-log(`slides: ${slides.children.length}`);
+// log(`slides: ${slides.children.length}`);
+// log(slides.children);
+const images = document.querySelectorAll(".slides img");
+
+function prevSlide() {
+	log(`prevSlide`);
+}
+let step = 120;
+let index = 0;
+let count = 0;
+// let rot = -50;
+let rot = [-50, 50, 0];
+function nextSlide() {
+	log(`nextSlide`);
+	slides.style.transform = `rotateY(${step}deg) translateX(${rot[count]}%)`;
+	// slides.style.transform = `rotateY(${step}deg)`;
+	step += 120;
+	index++;
+	count = index % 3;
+	// rot += 100;
+	// images.forEach((el, index) => {
+	// 	if (el.index === 0) {
+	// 		el.index = 3;
+	// 	}
+	// return step;
+}
+
+// setTimeout(() => {
+// if (step === 360) {
+// 	step = 0;
+// }
+// }, 300);
+// });
+// images.forEach((el, index) => {
+// 	log(el);
+// 	el.style.zIndex = "" + index;
+// });
+// log(images);
+// function addZindex(el) {
+// 	el.style.zIndex = 1;
+// }
+
+// images
+log(images);
+// images.forEach((el, index) => {
+// 	log(el);
+// 	el.style.zIndex = "" + index;
+// });
