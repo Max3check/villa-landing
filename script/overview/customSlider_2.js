1c5fd7e9aaef7b35cc7a0c45119d894903d14926
Maxim <maxtab1@mail.ru>
2024-03-06 06:49:21 +0300

    make working version of custom slider

--- script/overview/customSlider_2.js
+++ script/overview/customSlider_2.js
@@ -1,52 +1,33 @@
 const slides = document.querySelector(".slides");
 const images = document.querySelectorAll(".slides img");
 
-let step = 0;
-// let countStep = 120;
-let index = 0;
-let indexPrev = 0;
-let count = 0;
-let countPrev = 0;
-let rot = [0, -50, 50];
-//       360  120  240
-//       720  480  600
-//      1080  840  960
-// let rot = [-50, 50, 0, 0, -50, 50];
-// [0, -50, 50]
-let rotPrev = [50, -50, 0];
+let angle = 0;
+let shift = 0;
+let dataNext = [0, -50, 50];
+let dataPrev = [0, -50, 50];
 
 function prevSlide() {
-	log(`prevSlide`);
-	step -= 120;
-	// count === 0
-	// 	? (countPrev = 0)
-	// 	: count === 1
-	// 	? (countPrev = 2)
-	// 	: (countPrev = 1);
-	index--;
-	count = Math.abs(index + 3) % 3;
-	slides.style.transform = `rotateY(${step}deg) translateX(${rot[count]}%)`;
-	// index++;
-	// count = index % 3;
-	// indexPrev++;
-	// countPrev = indexPrev % 3;
-
-	log(`index prev: ${index}`);
-	log(`count prev: ${count}`);
+	angle -= 120;
+	shift = Math.abs((angle % 360) / 120);
+	if (angle < 0) {
+		slides.style.transform = `rotateY(${angle}deg) translateX(${
+			-1 * dataNext[shift]
+		}%)`;
+	} else {
+		slides.style.transform = `rotateY(${angle}deg) translateX(${dataNext[shift]}%)`;
+	}
 }
 
 function nextSlide() {
-	log(`nextSlide`);
-	step += 120;
-	index++;
-	count = index % 3;
+	angle += 120;
+	shift = Math.abs((angle % 360) / 120);
+	// slides.style.transform = `rotateY(${angle}deg) translateX(${dataNext[shift]}%)`;
 
-	slides.style.transform = `rotateY(${step}deg) translateX(${rot[count]}%)`;
-	// countStep = step % 360;
-	// countStep === 0 ? (countStep = 360) : null;
-	// index = count;
-	log(`index next: ${index}`);
-	log(`count next: ${count}`);
-	log(`step next: ${step}`);
+	if (angle < 0) {
+		slides.style.transform = `rotateY(${angle}deg) translateX(${
+			-1 * dataNext[shift]
+		}%)`;
+	} else {
+		slides.style.transform = `rotateY(${angle}deg) translateX(${dataNext[shift]}%)`;
+	}
 }
-// log(images);
