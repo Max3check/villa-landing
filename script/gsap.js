b9c80bc2e24ea2fb85e1f12e672634461872c64d
Maxim <maxtab1@mail.ru>
2024-03-08 21:04:55 +0300

    add GSAP

--- script/gsap.js
+++ script/gsap.js
@@ -1,1 +1,16 @@
 log("GSAP");
+// const log = console.log;
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".overview_typing", {
+	// x: 500,
+	// duration: 3,
+	scrollTrigger: {
+		trigger: ".overview_typing",
+		start: "top 70%",
+		// end: "bottom 50%",
+		// markers: true,
+		toggleClass: "rotateOnScroll",
+	},
+});
