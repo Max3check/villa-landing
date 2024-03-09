eb35dc6cb1f094b491eade22797145f98a9c40fc
Maxim <maxtab1@mail.ru>
2024-03-09 05:31:55 +0300

    regular

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
 const log = console.log;
+
+window.onload = () => {
+	let windowWidth = window.innerWidth;
+	let windowHeight = window.innerHeight;
+	info_window_id.innerText = `w: ${windowWidth} \n h: ${windowHeight}`;
+};
